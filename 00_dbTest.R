55b93389ad628083891b18a358c7682422640c36
takewiki <hulilei@takewiki.com.cn>
2020-05-01 19:20:46 +0800

    V1.1

--- 00_dbTest.R
+++ 00_dbTest.R
@@ -1,9 +1,20 @@
 library(tsda)
 library(tsdo)
+# startTime <-getTime()
+# conn_abc <- conn_rds('lcdb')
+# #test for t_icitem        21754* 215
+# item_sql <-"select * from t_ICItem"
+# item_res <- tsda::sql_select(conn_abc,item_sql)
+# endTime <-getTime()
+# item_time <- data.frame(startTime,endTime,stringsAsFactors = F)
+# View(item_time)
+
+# test for barcode
+# 594824*68
 startTime <-getTime()
 conn_abc <- conn_rds('lcdb')
 #test for t_icitem        21754* 215
-item_sql <-"select * from t_ICItem"
+item_sql <-"select * from T_SUF_Barcode"
 item_res <- tsda::sql_select(conn_abc,item_sql)
 endTime <-getTime()
 item_time <- data.frame(startTime,endTime,stringsAsFactors = F)
